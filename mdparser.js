b1b7e3592dc93ac982f29467cf2de8f04456a7d3
caeost <caeost@gmail.com>
2014-04-07 19:23:24 -0700

    initial commit

---
+++ mdparser.js
@@ -0,0 +1,86 @@
+var fs = require("fs"),
+    markdown = require( "markdown" ).markdown;
+
+
+var regex = /```([A-Za-z]*)?(-N)?([^```]*)```/g;
+
+//add more types later
+// https://github.com/github/linguist/blob/master/lib/linguist/languages.yml
+// (only web stuff should go into *name*-page format)
+var map = {
+  "javascript": "js",
+  "CSS": "css",
+  "HTML": "html"
+};
+
+var mdparser = function(filename, watch, generateHtml) {
+  this.fileName = filename;
+  this.watch = watch;
+  this.html = generateHtml;
+
+  this.read(filename);
+
+  if(watch) {
+    var that = this;
+    fs.watch(filename, function(e, name) {
+      that.read(filename);
+    });
+  } 
+};
+
+mdparser.prototype = {
+  generateHtml: function(data) {
+    var html = markdown.toHTML(data);
+    fs.writeFile(output, html, function(err) {
+      if(err) throw err;
+    });
+  },
+  read: function(filename) {
+    var that = this;
+    fs.readFile(filename, function (err, data) {
+      if (err) throw err;
+      that.operate(data, filename);
+      if(that.html) that.generateHtml(data);
+    });
+  },
+  operate: function(data, name) {
+    var parsedObject = this.parse(data);
+    for (var key in parsedObject) {
+      var parsed = parsedObject[key],
+          documentationCode = !~~key.indexOf("page-"),
+          type = documentationCode ? key.substring(5) : key,
+          output = name.split(".")[0] + (documentationCode ? "-page" : "") + "." + map[type];
+
+      if(documentationCode && !this.html) continue;
+
+      fs.writeFile(output, parsed, function(err) {
+        if(err) throw err;
+      });
+    }
+  },
+  parse: function(file) {
+    var myArray,
+        result = {};
+  
+    while ((myArray = regex.exec(file)) !== null) {
+      var text = "" + myArray[3],
+          noout = myArray[2],
+          //see note in mdparser bash script
+          type = myArray[1] ||  "javascript";
+  
+      if(!text.length) continue;
+      if(noout) type = "page-" + type 
+  
+      if(result[type]) {
+        result[type] += text;
+      } else {
+        result[type] = text;
+      }
+    } 
+    return result;
+  }
+}
+
+
+module.exports = mdparser;
+
